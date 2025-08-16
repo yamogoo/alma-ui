77fae8b584943b1e2d4a951fdbf29a877213a45a
mishagrebennikov <grebennikovmisha@gmail.com>
2025-08-16 12:01:42 +0300

    docs(input): add documentation and usage examples

---
+++ client/src/components/atoms/buttons/button.ts
@@ -0,0 +1,12 @@
+import type { Color, Size } from "./Button.vue";
+
+export const colors: Array<Color> = [
+  "primary",
+  "primary-inversed",
+  "secondary",
+  "accent",
+  "error",
+  "transparent",
+];
+
+export const sizes: Array<Size> = ["xs", "sm", "md", "lg"];
---
+++ client/src/components/atoms/inputs/PasswordInput.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
+
+import PasswordInput from "./PasswordInput.vue";
+import { inputColors, inputSizes } from "./input";
+
+const meta = {
+  title: "Atoms/inputs/PasswordInput",
+  component: PasswordInput,
+  tags: ["autodocs"],
+  argTypes: {
+    size: { control: "select", options: inputSizes },
+    color: {
+      control: "select",
+      options: inputColors,
+    },
+  },
+} satisfies Meta<typeof PasswordInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const InputLg: Story = {
+  args: {
+    size: "lg",
+    color: "primary",
+    value: "",
+    placeholder: "Password",
+  },
+};
+
+export const InputMd: Story = {
+  args: {
+    size: "md",
+    color: "primary",
+    value: "",
+    placeholder: "Password",
+  },
+};
